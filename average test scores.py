70b08437f9732cdf54706c4d2d31513b6984d2cc
DanLawrie <71726127+DanLawrie@users.noreply.github.com>
2020-09-29 14:33:01 -0400

    version 2

--- average test scores.py
+++ average test scores.py
@@ -1,6 +1,15 @@
+
+numberOfTest = 0
+score = 0
+scoreCount = 0
+total = 0
+average = 0
+
 numberOfTest = int(input("Please enter the number of tests you want to average "))
-score1 = float(input("Please enter a score "))
-score2 = float(input("Please enter a score "))
-average = (score1 + score2)/2 
+while(numberOfTest > scoreCount):
+    score1 = int(input("Please enter a score "))
+    scoreCount = scoreCount + 1
+    total = (score1 + total)
+average = total / numberOfTest
 print("the average is ", average)
              
